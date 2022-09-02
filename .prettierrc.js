13bb0159b0e2330746d86a34312f9140d093bf82
Евгений Иваницкий <evgenijivanickij@192.168.88.16>
2022-09-02 03:47:58 +0300

    prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 2
 };
