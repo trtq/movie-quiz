e6c36c81a31b5ae195737d11830b60bace848227
Евгений Иваницкий <tortique@Evgenijs-MacBook-Pro.local>
2022-09-05 18:26:55 +0300

    question generation functionality

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,7 @@
 declare module '@env' {
   export const SIZE_MATTERS_BASE_WIDTH: number;
   export const SIZE_MATTERS_BASE_HEIGHT: number;
+  export const PICTURE_URL_ROOT: string;
+  export const MOVIE_URL_ROOT: string;
+  export const MOVIEDB_API_KEY: string;
 }
--- src/screens/GameScreen/types.ts
+++ src/screens/GameScreen/types.ts
@@ -4,3 +4,16 @@ import { TQuizNavigatorStackParamList, SCREENS } from '@src/router/types';
 export type TGameScreenProps = {
   navigation: NativeStackNavigationProp<TQuizNavigatorStackParamList, SCREENS.Game>;
 };
+
+export type TQuestion = {
+  id: number;
+  picture: string;
+  correctName: string;
+  answers: TAnswer[];
+};
+
+export type TAnswer = {
+  id: number;
+  name: string;
+  correct: boolean;
+};
