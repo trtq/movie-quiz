ae51214811ba3185dccc088e1fc250b70f4f37be
Евгений Иваницкий <evgenijivanickij@192.168.88.16>
2022-09-06 19:51:39 +0300

    android theme fix

--- src/components/SettingsProvider/SettingsProvider.tsx
+++ src/components/SettingsProvider/SettingsProvider.tsx
@@ -4,6 +4,7 @@ import { StatusBar, Platform } from 'react-native';
 import { useStore } from '@src/stores/useStore';
 import { themes } from '@src/utils/themes/themes';
 import { ThemeProvider } from 'styled-components';
+import { THEME } from '@src/utils/themes/types';
 
 // a wrapper that reads AsyncStorage info at the point of entrance
 // sets up the StatusBar color for androids
@@ -20,6 +21,11 @@ export const SettingsProvider = observer(({ children }: { children: React.ReactN
   useEffect(() => {
     if (Platform.OS === 'android') {
       StatusBar.setBackgroundColor(themes[theme].background);
+      if (theme === THEME.Dark) {
+        StatusBar.setBarStyle('light-content');
+      } else {
+        StatusBar.setBarStyle('dark-content');
+      }
     }
   }, [theme]);
 
